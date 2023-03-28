bdf780427b76cf0aa049082de9636c0d063df2cb
long15053758403 <>
2023-03-28 16:30:24 +0800

    添加k线图组件

--- src/index.js
+++ src/index.js
@@ -14,9 +14,9 @@ root.render(
   // <BrowserRouter> 
   //   <App />
   // </BrowserRouter>
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
